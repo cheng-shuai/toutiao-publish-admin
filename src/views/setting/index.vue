a41a6e175ddcf875f206c93be0973290e22c08b1
cheng-shuai <me@cheng-shuai.top>
2021-02-02 20:49:24 +0800

    提交

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -2,27 +2,27 @@
   <div class="setting-container">
     <el-row :gutter="20">
       <el-col :span="12">
-          <el-form :model="userProfile" label-width="80px">
-            <el-form-item label="编号">
-              <el-input v-model="userProfile.id"></el-input>
-            </el-form-item>
-            <el-form-item label="手机">
-              <el-input v-model="userProfile.mobile"></el-input>
-            </el-form-item>
-            <el-form-item label="媒体名称">
-              <el-input v-model="userProfile.name"></el-input>
-            </el-form-item>
-            <el-form-item label="媒体介绍">
-              <el-input type="textarea" v-model="userProfile.intro"></el-input>
-            </el-form-item>
-            <el-form-item label="邮箱">
-              <el-input v-model="userProfile.email"></el-input>
-            </el-form-item>
-            <el-form-item>
-              <el-button type="primary" @click="onSubmit">保存设置</el-button>
-              <el-button>取消</el-button>
-            </el-form-item>
-          </el-form>
+        <el-form :model="userProfile" label-width="80px">
+          <el-form-item label="编号">
+            <el-input v-model="userProfile.id"></el-input>
+          </el-form-item>
+          <el-form-item label="手机">
+            <el-input v-model="userProfile.mobile"></el-input>
+          </el-form-item>
+          <el-form-item label="媒体名称">
+            <el-input v-model="userProfile.name"></el-input>
+          </el-form-item>
+          <el-form-item label="媒体介绍">
+            <el-input type="textarea" v-model="userProfile.intro"></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱">
+            <el-input v-model="userProfile.email"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">保存设置</el-button>
+            <el-button>取消</el-button>
+          </el-form-item>
+        </el-form>
       </el-col>
       <el-col :span="12" class="avatar-div">
         <label for="file">
@@ -30,12 +30,28 @@
             shape="square"
             :size="178"
             fit="cover"
-            :src="userProfile.photo"></el-avatar>
+            :src="previewImage"></el-avatar>
           <p>点击修改头像</p>
         </label>
-        <input type="file" id="file" hidden>
+        <input
+          type="file"
+          id="file"
+          ref="file"
+          hidden
+          @change="onFileChange">
       </el-col>
     </el-row>
+    <!--    弹出层-->
+    <el-dialog
+      title="更换头像"
+      append-to-body
+      :visible.sync="dialogVisible">
+      <img :src="previewImage" alt="" width="178" height="178">
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+  </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -56,7 +72,9 @@ export default {
         resource: '',
         desc: ''
       },
-      userProfile: {}
+      userProfile: {},
+      previewImage: '',
+      dialogVisible: false
     }
   },
   methods: {
@@ -67,6 +85,18 @@ export default {
       getUserProfile().then(res => {
         this.userProfile = res.data.data
       })
+    },
+    onFileChange () {
+      // 显示弹出层
+      this.dialogVisible = true
+
+      // 显示预览图片
+      const file = this.$refs.file
+      const blobData = window.URL.createObjectURL(file.files[0])
+      this.previewImage = blobData
+
+      // 结局相同文件不触发change事件
+      this.$refs.file.value = ''
     }
   },
   created () {

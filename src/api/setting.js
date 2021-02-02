48a7c17edad5d92769b02cb9e148adf6f7cb14c2
cheng-shuai <me@cheng-shuai.top>
2021-02-02 15:45:17 +0800

    个人设置页面布局完成

---
+++ src/api/setting.js
@@ -0,0 +1,25 @@
+/***
+ * 个人设置
+ */
+
+import request from '@/utils/request'
+
+/**
+ * 获取用户个人资料
+ */
+export const getUserProfile = () => {
+  return request({
+    method: 'GET',
+    url: '/mp/v1_0/user/profile'
+  })
+}
+
+/**
+ * 编辑用户头像
+ */
+export const uploadAvatar = (data) => {
+  return request({
+    method: 'PATCH',
+    url: '/mp/v1_0/user/photo'
+  })
+}
--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,13 +1,84 @@
 <template>
-<div class="setting-container">设置</div>
+  <div class="setting-container">
+    <el-row :gutter="20">
+      <el-col :span="12">
+          <el-form :model="userProfile" label-width="80px">
+            <el-form-item label="编号">
+              <el-input v-model="userProfile.id"></el-input>
+            </el-form-item>
+            <el-form-item label="手机">
+              <el-input v-model="userProfile.mobile"></el-input>
+            </el-form-item>
+            <el-form-item label="媒体名称">
+              <el-input v-model="userProfile.name"></el-input>
+            </el-form-item>
+            <el-form-item label="媒体介绍">
+              <el-input type="textarea" v-model="userProfile.intro"></el-input>
+            </el-form-item>
+            <el-form-item label="邮箱">
+              <el-input v-model="userProfile.email"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="onSubmit">保存设置</el-button>
+              <el-button>取消</el-button>
+            </el-form-item>
+          </el-form>
+      </el-col>
+      <el-col :span="12" class="avatar-div">
+        <label for="file">
+          <el-avatar
+            shape="square"
+            :size="178"
+            fit="cover"
+            :src="userProfile.photo"></el-avatar>
+          <p>点击修改头像</p>
+        </label>
+        <input type="file" id="file" hidden>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
+import { getUserProfile } from '@/api/setting'
+
 export default {
-  name: 'Setting'
+  name: 'Setting',
+  data () {
+    return {
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      },
+      userProfile: {}
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log(111)
+    },
+    loadUserProfile () {
+      getUserProfile().then(res => {
+        this.userProfile = res.data.data
+      })
+    }
+  },
+  created () {
+    this.loadUserProfile()
+  }
 }
 </script>
 
 <style lang="less" scoped>
-
+.avatar-div {
+  display: flex;
+  justify-content: center;
+  text-align: center;
+}
 </style>
